9e7a6af03c2f44f091d2cff30ade212e1e53cc25
Igor Androsov <iandrosov@salesforce.com>
2023-03-11 22:12:00 +0900

    Add language switch to Home page navi

--- assets/js/tcc.js
+++ assets/js/tcc.js
@@ -1,15 +1,19 @@
-
 //$(function(){
 //    $('.selectpicker').selectpicker();
 //});
-const switchLanguage = function(lang) {
-    alert(lang);
-    window.location.href = lang;
-    window.location = lang;
-}
-$(".nav").change(function() {
-    window.location.href = $(".nav").value;
-    window.location = $(".nav").val;
-    alert($(".nav").val());
-  });
-  
+const switchLanguage = function (lang) {
+  console.log(lang);
+  window.location.href = lang;
+  window.location = lang;
+};
+/*
+$(".nav").change(function () {
+  window.location.href = $(".nav").value;
+  window.location = $(".nav").val;
+  alert($(".nav").val());
+});*/
+const initPage = function () {
+  const queryString = window.location.search;
+  console.log(queryString);
+};
+initPage();
